0a1e544d9cb10ca4e6f6f1cc6d6975226cbe6b30
Stefan <stefan@studiokhi.com>
2025-01-15 20:01:01 +0100

    feat: responsive desktop

--- src/js/animation/text-reveal.js
+++ src/js/animation/text-reveal.js
@@ -4,12 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // PRELOADER
     // TITLE REVEAL
-
     const lines = document.querySelectorAll('.line')
     let i = 0
     lines.forEach((line) => {
         gsap.fromTo(line, {
-            y: (line.getBoundingClientRect().height + 20),
+            y: (line.getBoundingClientRect().height * 1.6),
         }, {
             y: 0,
             duration: 3,
